3b537d4b9236d2eec1de5f64d4215be02ca852dc
VishalKumar-B <73609796+VishalKumar-B@users.noreply.github.com>
2021-01-20 11:00:49 +0530

    second

---
+++ HibernateWithOutUsingXML/src/com/motivity/DBConnection.java
@@ -0,0 +1,31 @@
+package com.motivity;
+
+import java.util.Properties;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DBConnection {
+
+	public static Session con() {
+		Properties p = new Properties();
+		p.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+		p.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/motivity");
+		p.setProperty("hibernate.connection.username", "root");
+		p.setProperty("hibernate.connection.password", "root");
+		p.setProperty("dialet", "org.hibernate.dialect.MySQL8Dialect");
+		p.setProperty("hibernate.show_sql", "true");
+		p.setProperty("hibernate.hbm2ddl.auto", "create");
+
+		Configuration cf = new Configuration();
+		cf.addProperties(p);
+		cf.addAnnotatedClass(StudentTwoPOJO.class);
+		cf.addAnnotatedClass(EmployeeTwo.class);
+
+		SessionFactory sf = cf.buildSessionFactory();
+		Session se = sf.openSession();
+
+		return se;
+	}
+}
--- HibernateWithOutUsingXML/src/com/motivity/MainLogic.java
+++ HibernateWithOutUsingXML/src/com/motivity/MainLogic.java
@@ -1,9 +1,5 @@
 package com.motivity;
 
-import java.util.Properties;
-
-import org.hibernate.cfg.Configuration;
-import org.hibernate.SessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -11,22 +7,7 @@ public class MainLogic {
 
 	public static void main(String[] args) {
 
-		Properties p = new Properties();
-		p.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-		p.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/motivity");
-		p.setProperty("hibernate.connection.username", "root");
-		p.setProperty("hibernate.connection.password", "root");
-		p.setProperty("dialet", "org.hibernate.dialect.MySQL8Dialect");
-		p.setProperty("hibernate.show_sql", "true");
-		p.setProperty("hibernate.hbm2ddl.auto", "create");
-
-		Configuration cf = new Configuration();
-		cf.addProperties(p);
-		cf.addAnnotatedClass(StudentTwoPOJO.class);
-		cf.addAnnotatedClass(EmployeeTwo.class);
-
-		SessionFactory sf = cf.buildSessionFactory();
-		Session se = sf.openSession();
+		Session se = DBConnection.con();
 		Transaction tx = se.beginTransaction();
 
 		StudentTwoPOJO s = new StudentTwoPOJO();
@@ -43,7 +24,6 @@ public class MainLogic {
 
 		tx.commit();
 		se.close();
-		sf.close();
 	}
 
 }
