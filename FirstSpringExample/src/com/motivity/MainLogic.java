ff7e32e9380d285561d85cf1abe2daccfaf94e75
VishalKumar-B <73609796+VishalKumar-B@users.noreply.github.com>
2021-01-29 10:09:22 +0530

    commit

---
+++ FirstSpringExample/src/com/motivity/MainLogic.java
@@ -0,0 +1,18 @@
+package com.motivity;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class MainLogic {
+
+	public static void main(String[] args) {
+		
+		Resource res = new ClassPathResource("spring.xml");
+		BeanFactory factory = new XmlBeanFactory(res);
+		Object o = factory.getBean("rahul");
+		Student x = (Student)o;
+		System.out.println(x.getSid()+" "+x.getSname()+" "+x.getSmarks());
+	}
+}
---
+++ FirstSpringExample/src/com/motivity/Student.java
@@ -0,0 +1,32 @@
+package com.motivity;
+
+public class Student {
+
+	private int sid;
+	private String sname;
+	private String smarks;
+
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public String getSmarks() {
+		return smarks;
+	}
+
+	public void setSmarks(String smarks) {
+		this.smarks = smarks;
+	}
+}
